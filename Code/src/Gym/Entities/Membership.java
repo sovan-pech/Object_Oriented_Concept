3c03c8b35e4eeec7d445bbf3c9579e211a19cab3
Sovan Pech <pech.sovan.arc@gmail.com>
2026-04-28 09:54:49 +0700

    New Function in Membership

--- Code/src/Gym/Entities/Membership.java
+++ Code/src/Gym/Entities/Membership.java
@@ -13,4 +13,70 @@ public class Membership {
   private String endDate;
   private MemberStatus status;
 
+  // constructor
+  public Membership(Member member, MembershipPlan plan, String startDate, String endDate, MemberStatus status) {
+    this.membershipId = membershipId;
+    this.member = member;
+    this.plan = plan;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.status = status;
+  }
+
+  // Getters and Setters
+
+  public void setMembershipId(String membershipId) {
+    this.membershipId = membershipId;
+  }
+
+  public Member getMember() {
+    return member;
+  }
+
+  public void setMember(Member member) {
+    this.member = member;
+  }
+
+  public MembershipPlan getPlan() {
+    return plan;
+  }
+
+  public void setPlan(MembershipPlan plan) {
+    this.plan = plan;
+  }
+
+  public String getStartDate() {
+    return startDate;
+  }
+
+  public void setStartDate(String startDate) {
+    this.startDate = startDate;
+  }
+
+  public String getEndDate() {
+    return endDate;
+  }
+
+  public void setEndDate(String endDate) {
+    this.endDate = endDate;
+  }
+
+  public MemberStatus getStatus() {
+    return status;
+  }
+
+  public void setStatus(MemberStatus status) {
+    this.status = status;
+  }
+
+  // Display method
+  public void displayMembershipInfo() {
+    System.out.println("Membership ID: " + membershipId);
+    System.out.println("Member: " + member);
+    System.out.println("Plan: " + plan);
+    System.out.println("Start Date: " + startDate);
+    System.out.println("End Date: " + endDate);
+    System.out.println("Status: " + status);
+  }
+
 }
