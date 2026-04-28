cd419b01a43285e9e4d641a30224127806e81723
Sovan Pech <pech.sovan.arc@gmail.com>
2026-04-28 09:41:47 +0700

    “…”

---
+++ Code/src/Gym/Entities/Membership.java
@@ -0,0 +1,16 @@
+package Gym.Entities;
+
+import java.lang.reflect.Member;
+
+import Gym.Enum.MemberStatus;
+
+public class Membership {
+
+  private String membershipId;
+  private Member member;
+  private MembershipPlan plan;
+  private String startDate;
+  private String endDate;
+  private MemberStatus status;
+
+}
