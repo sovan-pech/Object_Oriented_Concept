5047215e96d24f760e636c8a2eaa3a99e681d166
Sovan Pech <pech.sovan.arc@gmail.com>
2026-04-29 22:08:42 +0700

    New

--- Code/src/Gym/Runner/GymManagement.java
+++ Code/src/Gym/Runner/GymManagement.java
@@ -1,4 +1,3 @@
-package Gym.Runner;
 
 import Gym.Entities.Membership;
 import Gym.Enum.PaymentMethod;
@@ -12,51 +11,55 @@ import Gym.Service.StaffService;
 import java.util.Scanner;
 
 public class GymManagement {
-    public  static  final String gymName="SEBA-FITNESS";
+    public static final String gymName = "SEBA-FITNESS";
     private MemberService memberService = new MemberService();
     private StaffService staffService = new StaffService();
-    private PaymentService paymentService= new PaymentService();
+    private PaymentService paymentService = new PaymentService();
     private MembershipService membershipService = new MembershipService();
     Scanner input = new Scanner(System.in);
-    public void run(){
-        int choice=-1;
-        System.out.println("Welcome To "+gymName);
-        do{
+
+    public void run() {
+        int choice = -1;
+        System.out.println("Welcome To " + gymName);
+        do {
             System.out.println(
                     """
                             \tMain Menu
                             1.Manager Members
                             2.Manage Payment
                             0.Exit!
-                            """
-            );
-            System.out.print("Enter Choice : ");  choice=input.nextInt();input.nextLine();
-            switch (choice){
-                case 1-> memberMenu();
-                case 2-> paymentMenu();
+                            """);
+            System.out.print("Enter Choice : ");
+            choice = input.nextInt();
+            input.nextLine();
+            switch (choice) {
+                case 1 -> memberMenu();
+                case 2 -> paymentMenu();
                 case 0 -> System.out.println("Good Luck");
 
             }
-        }while(choice !=0);
+        } while (choice != 0);
     }
-    private void memberMenu(){
+
+    private void memberMenu() {
         int choice = -1;
         do {
             System.out.println("""
-                \n====== MEMBER MENU ======
-                1. Add Member
-                2.Add Membership
-                2. View All Members
-                0. Back
-                =========================""");
+                    \n====== MEMBER MENU ======
+                    1. Add Member
+                    2.Add Membership
+                    2. View All Members
+                    0. Back
+                    =========================""");
             System.out.print("Enter choice: ");
-            choice =input.nextInt(); input.nextLine();
+            choice = input.nextInt();
+            input.nextLine();
             switch (choice) {
                 case 1 -> {
                     Members m = memberService.createMember(input);
                     memberService.addMember(m);
                 }
-                case 2-> {
+                case 2 -> {
 
                 }
                 case 3 -> {
@@ -67,17 +70,19 @@ public class GymManagement {
             }
         } while (choice != 0);
     } // end member menu
-    private void paymentMenu(){
+
+    private void paymentMenu() {
         int choice = -1;
         do {
             System.out.println("""
-                \n====== PAYMENT MENU ======
-                1. Process Payment
-                2. View All Payments
-                0. Back
-                ==========================""");
+                    \n====== PAYMENT MENU ======
+                    1. Process Payment
+                    2. View All Payments
+                    0. Back
+                    ==========================""");
             System.out.print("Enter choice: ");
-            choice = input.nextInt(); input.nextLine();
+            choice = input.nextInt();
+            input.nextLine();
 
             switch (choice) {
                 case 1 -> {
@@ -86,9 +91,11 @@ public class GymManagement {
                     Membership m = membershipService.findByID(input.nextLine());
                     if (m == null) {
                         System.out.println("Member not found.");
-                        break; }
+                        break;
+                    }
                     System.out.print("Enter Discount (0 for none): ");
-                    float discount = input.nextFloat();input.nextLine();
+                    float discount = input.nextFloat();
+                    input.nextLine();
                     System.out.print("Enter Payment Method (KHQR/BYCASH/CREDITCARD): ");
                     PaymentMethod method = PaymentMethod.valueOf(input.nextLine().toUpperCase());
                     paymentService.processPayment(m, discount, method);
